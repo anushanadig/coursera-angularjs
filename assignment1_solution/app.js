9b209eac8842756bce3bc28e61c633a529a2e652
unknown <nshndg@XWJPMDD307.its.corp.gwl.com>
2017-11-28 16:10:45 +0530

    assignment1_solution_modified

--- assignment1_solution/app.js
+++ assignment1_solution/app.js
@@ -8,22 +8,29 @@
     function foodController($scope){
         $scope.foodList = "";
         $scope.message = "";
+        //$scope.state = false;
         $scope.checkList = function(){
             var sep = ",";
             //console.log($scope.foodList);
             var splits = ($scope.foodList).split(',');
-            //console.log(splits);
+
+             for( var i = 0; i < splits.length; i++){
+                if(splits[i] == "") 
+                  splits.splice(i,1);
+            } 
+
+            console.log(splits);
             var len = splits.length;
             //console.log(len);
 
             //to handle a case where there is no item between some commas
-            for( var i = 0; i < len; i++){
+            /* for( var i = 0; i < len; i++){
                 if(splits[i] === "") 
                     len = len-1;
-            }
+            } */
 
             //textbox is empty or a string with just spaces in it
-            if(splits[0] === ""){
+            if(!$scope.foodList){
                 $scope.message = "Please enter data first";
                 $scope.state = false;
             }
